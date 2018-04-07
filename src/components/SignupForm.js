aca085b3d918792f35c30026d829c35ca628f36c
weeznog <drew.m.peterson@gmail.com>
2018-04-06 17:14:38 -0700

    add recompose handleChange

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withStateHandlers, compose } from 'recompose';
 import { Input, Button } from '../common';
 import { emailValidation, passwordValidation } from '../helpers/validation';
 import styled from 'styled-components';
@@ -6,46 +7,64 @@ import styled from 'styled-components';
 class SignupForm extends Component {
   state = {
     email: '',
-    password: '',
-    formInvalid: true,
-    errors: {
-      email: '',
-      password: ''
-    }
+    password: ''
   };
 
   onSubmit(event) {
     event.preventDefault();
-    console.log('state', this.state);
+    // const { email, password } = this.state;
+    console.log('this.props', this.props);
   }
 
-  handleChange({ target: { name, value } }) {
-    this.setState({ [name]: value });
-  }
+  // handleChange({ target: { name, value } }) {
+  //   let errors = {};
+  //   let error;
+  //   const meta = { ...this.state.meta };
+  //   this.setState({ [name]: value });
+  //
+  //   if (name === 'email') {
+  //     error = emailValidation(value);
+  //   } else {
+  //     error = passwordValidation(value);
+  //   }
+  //
+  //   if (error) {
+  //     errors = { ...this.state.meta.errors, ...error };
+  //     meta.errors = errors;
+  //     this.setState({ meta });
+  //     console.log('this.state', this.state);
+  //   }
+  // }
 
-  handleBlur({ target: { name, value } }) {}
+  // handleBlur({ target: { name, value } }) {}
 
   render() {
-    const { email, password, formInvalid, errors } = this.state;
+    const {
+      email,
+      password,
+      handleBlur,
+      handleChange,
+      formInvalid
+    } = this.props;
     return (
       <Form onSubmit={this.onSubmit.bind(this)}>
         <Input
           type="email"
           name="email"
-          value={email}
-          error={errors.email}
+          value={email.value}
+          // error={errors.email}
           placeholder="Email"
-          handleChange={this.handleChange.bind(this)}
-          handleBlur={this.handleBlur.bind(this)}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
         />
         <Input
           type="password"
           name="password"
-          value={password}
-          error={errors.password}
+          value={password.value}
+          // error={errors.password}
           placeholder="Password"
-          handleChange={this.handleChange.bind(this)}
-          handleBlur={this.handleBlur.bind(this)}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
           style={{ marginTop: '15px' }}
         />
 
@@ -72,4 +91,30 @@ const Form = styled.form`
   margin: 0 auto;
 `;
 
-export default SignupForm;
+export default compose(
+  withStateHandlers(
+    {
+      formInvalid: false,
+      email: {
+        value: '',
+        error: '',
+        touched: false
+      },
+      password: {
+        value: '',
+        error: '',
+        touched: false
+      }
+    },
+    {
+      handleBlur: props => event => {
+        console.log('props', props);
+        console.log('event', event);
+      },
+      handleChange: props => ({ target: { value, name } }) => {
+        const update = { ...props[name], value };
+        return { [name]: update };
+      }
+    }
+  )
+)(SignupForm);
--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,7 +1,17 @@
 export const emailValidation = value => {
-  console.log('email value', value);
+  if (!value) {
+    return { email: 'email is required' };
+  }
+  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    return { email: 'Invalid email address' };
+  }
 };
 
 export const passwordValidation = value => {
-  console.log('password value', value);
+  if (!value) {
+    return { password: 'Password is required' };
+  }
+  if (value.length < 6) {
+    return { password: 'Password much be atleast 6 characters long' };
+  }
 };
